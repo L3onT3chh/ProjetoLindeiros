19e80749844b1e43143e5065e794d9dde1a5a269
Gustavo Silva Quieregato <gustavo@siliconvillage.dev>
2022-05-16 14:57:05 -0300

    Change alterad

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
     <nav className="bgcolor-secondary">
       <ul>
         <Link to="/">
-          <li>Home 2.0</li>
+          <li>Home</li>
         </Link>
         <Link to="/eixos">
           <li>Eixos</li>
